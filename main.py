8e8fee3958410097803c86a3f4cc6d8f7b97c544
Anna <334230@niuitmo.ru>
2024-01-11 20:02:31 +0300

    1

--- main.py
+++ main.py
@@ -4,4 +4,36 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv('go_emotions_dataset.csv')
-print(df.head(5))
+print(df.head())
+print(df.columns)
+df = df.drop('id', axis=1)
+df = df.drop('example_very_unclear', axis=1)
+print(df)
+
+# Выделяем основные эмоции
+
+# SADNESS
+# Заменяем значения в столбце 'sadness'
+df['sadness'] = df[['disappointment', 'grief', 'remorse', 'sadness']].any(axis=1).astype(int)
+# Удаляем столбцы 'disappointment', 'grief', 'remorse'
+df = df.drop(['disappointment', 'grief', 'remorse'], axis=1)
+
+# FEAR
+# Заменяем значения в столбце 'fear'
+df['fear'] = df[['fear', 'embarrassment', 'nervousness', 'confusion']].any(axis=1).astype(int)
+# Удаляем столбцы 'embarrassment', 'nervousness', 'confusion'
+df = df.drop(['embarrassment', 'nervousness', 'confusion'], axis=1)
+
+# CALMNESS
+# Создаем новый столбец, т.к. среди имеющихся нет подходящего
+df = df.assign(сalmness=0)
+# Заменяем значения в столбце 'сalmness'
+df['сalmness'] = df[['сalmness', 'approval', 'caring',
+                     'desire', 'gratitude', 'love',
+                     'optimism', 'pride', 'relief',
+                     'realization', 'neutral']].any(axis=1).astype(int)
+# Удаляем все столбцы кроме calmness
+df = df.drop(['approval', 'caring',
+                     'desire', 'gratitude', 'love',
+                     'optimism', 'pride', 'relief',
+                     'realization', 'neutral'], axis=1)
