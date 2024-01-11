a7a6918e3ce27f02a2344297eda7bc52ce356496
Anna <334230@niuitmo.ru>
2024-01-11 19:34:11 +0300

    1

--- main.py
+++ main.py
@@ -2,16 +2,6 @@
 ##2##  git clone https://huggingface.co/datasets/go_emotions
 import pandas as pd
 import numpy as np
-import pyarrow.parquet as pq
 
-# Загрузка данных из файла Parquet
-table = pq.read_table('go_emotions/simplified/validation-00000-of-00001.parquet')
-
-# Преобразование данных в pandas DataFrame
-df = table.to_pandas()
-
-# Вывод первых нескольких строк данных
-print(df.head())
-print(len(df))
-print("Column headers from list(df.columns.values):",
-      list(df.columns.values))
+df = pd.read_csv('go_emotions_dataset.csv')
+print(df.head(5))
