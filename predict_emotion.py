c7cac7f8f27dafde631ae80097dc2c85b9f157dd
nik.tereshchenko.982@gmail.com <mama89526988485A!>
2024-01-15 03:31:30 +0300

    Теперь предсказание моделью сделано в виде функции и точно возвращает эмоцию(при этом сравнивает значение в словаре и выбирает максимальное значение из всех)

--- predict_emotion.py
+++ predict_emotion.py
@@ -10,7 +10,6 @@ from gru_network_training import tokenizer, embedding_dim, gru_units, emotions_l
 max_sequence_length = 100
 
 
-# Предобработка введенного текста
 def preprocess_text(text, tokenizer, stop_words):
     filtered_text = ' '.join([word for word in text.split() if word.lower() not in stop_words])
     sequence = tokenizer.texts_to_sequences([filtered_text])
@@ -18,21 +17,20 @@ def preprocess_text(text, tokenizer, stop_words):
     return padded_sequence
 
 
-# Загрузка обученной модели и векторизатора
-best_model = Sequential()
-best_model.add(
-    Embedding(input_dim=len(tokenizer.word_index) + 1, output_dim=embedding_dim, input_length=max_sequence_length))
-best_model.add(GRU(units=gru_units))
-best_model.add(Dense(units=len(emotions_labels.columns), activation='sigmoid'))
-best_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-best_model.load_weights('best_model_weights.h5')
+def predict_emotion(user_input):
+    best_model = Sequential()
+    best_model.add(
+        Embedding(input_dim=len(tokenizer.word_index) + 1, output_dim=embedding_dim, input_length=max_sequence_length))
+    best_model.add(GRU(units=gru_units))
+    best_model.add(Dense(units=len(emotions_labels.columns), activation='sigmoid'))
+    best_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    best_model.load_weights('best_model_weights.h5')
 
-# Предсказание настроения для введенного текста
-user_input = input("Введите текст: ")
-preprocessed_input = preprocess_text(user_input, tokenizer, stop_words)
-prediction = best_model.predict(preprocessed_input)
+    preprocessed_input = preprocess_text(user_input, tokenizer, stop_words)
+    prediction = best_model.predict(preprocessed_input)
 
-# Вывод предсказания
-emotions = ['anger', 'fear', 'joy', 'sadness', 'сalmness']
-predicted_emotions = {emotion: pred for emotion, pred in zip(emotions, prediction[0])}
-print("Предсказанные эмоции:", predicted_emotions)
+    max_emotion = max(emotions_labels.columns,
+                      key=lambda emotion: prediction[0][emotions_labels.columns.get_loc(emotion)])
+
+    return max_emotion
+
